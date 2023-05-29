0730fd8a4f43c66bfd87a6a66885326ebfd263e2
Bahadır AKMAN <bahadirakman55@gmail.com>
2023-05-29 10:45:30 +0300

    Cafe Rezervasyon Sitesi
    Asp.Net CORE MVC ile Cafe Rezervasyon Sitesi

---
+++ Cafe/Cafe/Migrations/20230313112141_AddContacts.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cafe.Migrations
+{
+    public partial class AddContacts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Blog",
+                table: "Blog");
+
+            migrationBuilder.RenameTable(
+                name: "Blog",
+                newName: "Blogs");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Blogs",
+                table: "Blogs",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefon = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Mesaj = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Tarih = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacts");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Blogs",
+                table: "Blogs");
+
+            migrationBuilder.RenameTable(
+                name: "Blogs",
+                newName: "Blog");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Blog",
+                table: "Blog",
+                column: "Id");
+        }
+    }
+}
